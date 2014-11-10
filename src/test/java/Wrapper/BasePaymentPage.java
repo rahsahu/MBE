1cca0bf54d48fda71827ef7d4a91a20f4ad9ad4e
Rahul Sahu <Rahul.Sahu@tomtom.com>
2014-11-10 20:09:56 +0530

    updated XPATH
    Signed-off-by: Rahul Sahu <Rahul.Sahu@tomtom.com>

--- src/test/java/Wrapper/BasePaymentPage.java
+++ src/test/java/Wrapper/BasePaymentPage.java
@@ -27,7 +27,7 @@ public class BasePaymentPage {
 	public By PaypalFormHeading=By.xpath("//div[@id='tab_paypal']/h3");
 
 	public By googleWalletSubmitButton=By.xpath("//div[@id='tab_google_wallet']");
-	public By googleWalletHeading=By.xpath("//img[@alt='Log In with Google Wallet']/h3");
+	public By googleWalletHeading=By.xpath("//div[@id='tab_google_wallet']/h3");
 	
 	
 //	Terms and Conditions 
